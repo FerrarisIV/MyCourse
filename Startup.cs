01ca0e00da7ca83ae8e0b8915bdddcb4d1a0001f
FerrarisIV <mauro.diiasio@gmail.com>
2019-12-21 18:15:42 +0100

    Aggiunto MVC Core

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,12 +33,11 @@ namespace MyCourse
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    string nome = context.Request.Query["nome"]; //parametro di provino
-                    await context.Response.WriteAsync($"Hello World mio come stai?!?? Io sono {nome}");
-                });
-            });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });    
         }
     }
 }
