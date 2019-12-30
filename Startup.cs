8c119cf0e0c1414d58b6380772bb6047c86432ae
FerrarisIV <mauro.diiasio@gmail.com>
2019-12-30 15:40:56 +0100

    struttura della pagina del catalogo

--- Startup.cs
+++ Startup.cs
@@ -17,6 +17,8 @@ namespace MyCourse
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
