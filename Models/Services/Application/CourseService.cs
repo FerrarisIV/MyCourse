c304cd80af1b42be44182f46dbf95a717a68525a
FerrarisIV <mauro.diiasio@gmail.com>
2020-01-04 11:04:16 +0100

    Aggiunta pagina di Dettaglio

--- Models/Services/Application/CourseService.cs
+++ Models/Services/Application/CourseService.cs
@@ -1,13 +1,61 @@
 using System;
 using System.Collections.Generic;
+using MyCourse.Models.Enums;
+using MyCourse.Models.ValueTypes;
 using MyCourse.Models.ViewModels;
 
 namespace MyCourse.Models.Services.Application {
     public class CourseService
     {
-        public List<CourseViewModel> GetServices()
+        public List<CourseViewModel> GetCourses()
         {
-            throw new NotImplementedException();
+            var CourseList = new List<CourseViewModel>();
+            var rand = new Random();
+            for (int i = 1; i <= 20; i++)
+            {
+                var price = Convert.ToDecimal(rand.NextDouble() * 10 + 10);
+                var course = new CourseViewModel
+                {
+                    Id = i,
+                    Title = $"Corso {i}",
+                    CurrentPrice = new Money(Currency.EUR, price),
+                    FullPrice = new Money(Currency.EUR, rand.NextDouble() > 0.5 ? price: price - 1),
+                    Author = "Nome e Cognome",
+                    Rating = rand.NextDouble() * 5.0,
+                    ImagePath = "/logo.png"
+                };
+                CourseList.Add(course);
+            }
+            return CourseList;
+        }
+
+        public CourseDetailViewModel GetCourse(int id)
+        {
+            var rand = new Random();
+            var price=Convert.ToDecimal(rand.NextDouble() * 10 + 10);
+            var course = new CourseDetailViewModel
+            {
+                Id = id,
+                Title = $"Corso {id}",
+                CurrentPrice = new Money(Currency.EUR, price),
+                FullPrice = new Money(Currency.EUR, rand.NextDouble() > 0.5 ? price: price - 1),
+                Author = "Nome e Cognome",
+                Rating = rand.Next(10, 50) / 10.0,
+                ImagePath = "/logo.png",
+                Description = $"Descrizione {id}",
+                Lessons = new List<LessonViewModel>()
+            };
+
+            for (var i = 1; i <= 5; i++)
+            {
+                var lesson = new LessonViewModel
+                {
+                    Title = $"Lezione {i}",
+                    Duration = TimeSpan.FromSeconds(rand.Next(40,90))
+                };
+                course.Lessons.Add(lesson);
+            }
+            return course;
         }
     }
 }
