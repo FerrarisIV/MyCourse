ef2d6832ff84e259535fc4249c6950bc34730327
FerrarisIV <mauro.diiasio@gmail.com>
2020-01-18 10:48:52 +0100

    Pagina di dettaglio

--- Models/Services/Application/EfCoreCourseService.cs
+++ Models/Services/Application/EfCoreCourseService.cs
@@ -15,22 +15,49 @@ namespace MyCourse.Models.Services.Application
         {
             this.dbContext = dbContext;
         }
-        public Task<CourseDetailViewModel> GetCourseAsync(int id)
+        public async Task<CourseDetailViewModel> GetCourseAsync(int id)
         {
-            throw new System.NotImplementedException();
+            CourseDetailViewModel ViewModel = await dbContext.Courses
+                .Where(course => course.Id == id)
+                .Select(course => new CourseDetailViewModel
+                {
+                    Id = course.Id,
+                    Title = course.Title,
+                    ImagePath = course.ImagePath,
+                    Author = course.Author,
+                    Description = course.Description,
+                    Rating = course.Rating,
+                    CurrentPrice = course.CurrentPrice,
+                    FullPrice = course.FullPrice,
+                    Lessons = course.Lessons.Select(lesson => new LessonViewModel
+                    {
+                        Id = lesson.Id,
+                        Title = lesson.Title,
+                        Description = lesson.Description,
+                        Duration = lesson.Duration
+                    })
+                    .ToList()
+                })
+                //.FirstOrDefaultAsync(); è il più tollerante tra i metodi, restituisce null se l'elenco è vuoto e non solleva mai un'eccezione
+                //.FirstAsync(); FirstAsync restituisce, di un elenco, il primo elmento, sollevando un'eccezione solo se l'elenco è vuoto.
+                //.SingleOrDefaultAsync(); a differenza di SingleAsync tollera il fatto che l'elenco sia vuoto (restituendo null)
+                .AsNoTracking()
+                .SingleAsync(); //SingleAsync solleva invece un'eccezione se il risultato NON è 1
+
+            return ViewModel;
         }
 
         public async Task<List<CourseViewModel>> GetCoursesAsync()
         {
-            List<CourseViewModel> courses = await dbContext.Courses.Select(course =>
-            new CourseViewModel {
-                Id = course.Id,
-                Title = course.Title,
-                ImagePath = course.ImagePath,
-                Author = course.Author,
-                Rating = course.Rating,
-                CurrentPrice = course.CurrentPrice,
-                FullPrice = course.FullPrice
+            List<CourseViewModel> courses = await dbContext.Courses
+                .Select(course => new CourseViewModel {
+                    Id = course.Id,
+                    Title = course.Title,
+                    ImagePath = course.ImagePath,
+                    Author = course.Author,
+                    Rating = course.Rating,
+                    CurrentPrice = course.CurrentPrice,
+                    FullPrice = course.FullPrice
             })
             .AsNoTracking()
             .ToListAsync();
