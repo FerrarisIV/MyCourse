9dcd7eb18d7f8e9e4c254837634d44d7a39f22c8
FerrarisIV <mauro.diiasio@gmail.com>
2020-01-27 16:26:26 +0100

    Sezione 12, creazione file appsettings.json

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ namespace MyCourse
         }
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
